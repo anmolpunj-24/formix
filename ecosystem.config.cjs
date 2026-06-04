5302a33913993f8b6065c3a0db2bca52582fe7b5
anmolpunj-24 <anmolpunj24@gmail.com>
2026-06-04 20:22:43 +0530

    deploy git actions

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "formix",
+      cwd: "C:/Users/anmol/Documents/formix",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start",
+      interpreter: "node"
+    }
+  ]
+};
